31af78eeebe409fb333e7faf75a5090fbf5834f8
Javier Caballero <paxet@users.noreply.github.com>
2018-11-21 08:53:26 +0100

    Remove creation of fish_greeting

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -5,17 +5,6 @@ if not functions -q fisher
     fish -c fisher
 end
 
-set mygreet "function fish_greeting
-    set_color \$fish_color_autosuggestion
-    echo (date \"+%H:%M:%S\")' | '(uname -o)' | '(whoami)'@'(hostname)
-    set_color normal
-end"
-if test ! -f ~/.config/fish/functions/fish_greeting.fish
-    # If no greeting, set it to show: machine name, users and OS
-    eval $mygreet
-    funcsave fish_greeting
-end
-
 # Set this to tell activate.fish in virtualenv to don't echo name in prompt
 set VIRTUAL_ENV_DISABLE_PROMPT 1
 
