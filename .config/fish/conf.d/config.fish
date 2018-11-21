fa9503870c23b48fff73cda034b598de7d056e65
Javier Caballero <paxet@users.noreply.github.com>
2018-11-21 09:30:33 +0100

    If poetry is installed, create completions file

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,9 +1,27 @@
-# Configure fisherman as plugin system for fish and some plugins
+set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+# Configure fisher as plugin system for fish and some plugins
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+if type -q poetry
+    if test ! -f $XDG_CONFIG_HOME/fish/completions/poetry.fish
+        mkdir --parents $XDG_CONFIG_HOME/fish/completions/
+        poetry completions fish > $XDG_CONFIG_HOME/fish/completions/poetry.fish
+        switch $SHELL
+            case /usr/bin/fish /bin/fish /usr/local/bin/fish
+                builtin source ~/.config/fish/completions/poetry.fish
+            case '*'
+                # The first time with fish
+                echo "### SHELL variable not pointing at fish. Be sure it's your default shell or"
+                echo "    export SHELL=/bin/fish before launching Fish Shell"
+                echo "### Poetry completion will be enabled in future fish sessions and needs the"
+                echo "    SHELL variable set to fish to work properly."
+        end
+    end
+end
+        
 
 # Set this to tell activate.fish in virtualenv to don't echo name in prompt
 set VIRTUAL_ENV_DISABLE_PROMPT 1
